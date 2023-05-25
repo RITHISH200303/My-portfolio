1958981f679bba186694293c58af7e55648feb5d
RITHISH200303 <rithish200303@gmail.com>
2023-05-26 02:12:07 +0530

    fourth commit

---
+++ js/gridScroll.js
@@ -0,0 +1,9 @@
+const gridContainer = document.getElementById("projects-container");
+const gridLength = gridContainer.childElementCount;
+const sevalueOne = [1, 1];
+const setValueTwo = [4, 3];
+let summa = 1;
+// gridContainer.style.gridTemplateRows = `repeat(${gridLength / 4}, ${"150px"})`;
+// for(i=1; i<=gridLength;i++){
+//    if
+// }
